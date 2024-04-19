6fb7e49f17e14848e10f7d0386fc5deac46cd4db
FValerio96 <fla.valerio.96@gmail.com>
2024-04-19 15:06:44 +0200

    utilizzato un time.sleep posto a 5 secondi per contrastare la lentezza del server nel fornirmi la pagina ed evitando dunque di avere un ri-uso di link o testi precedenti.

--- Selenium.py
+++ Selenium.py
@@ -4,12 +4,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
+
 
 def takeHTMLFromUrl(url, driver):
     try:
         findTextInClassBS(url, "bodyTesto", driver)
         url = driver.current_url
         while articoloSuccessivov2(driver, url):
+            time.sleep(4)
             findTextInClassBS(url, "bodyTesto", driver)
             url = driver.current_url
 
@@ -44,6 +47,8 @@ def findTextInClassBS(url, className, driver):
     # Effettua la richiesta GET alla pagina
     driver.get(url)
 
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+
     pageSource = driver.page_source
     soup = BeautifulSoup(pageSource, 'html.parser')
 
@@ -65,21 +70,25 @@ def articoloSuccessivov2(driver, url):
             EC.presence_of_all_elements_located((By.XPATH, '//a[@class="btn"]'))
         )
 
-        # Se ci sono almeno due link, clicca sul secondo link
-        if len(links) >= 2 and links[1].accessible_name == "articolo successivo":
-            links[1].click()
+        linkToClick = None
+
+        for link in links:
+            if link.accessible_name == "articolo successivo":
+                linkToClick = link
+                break
+
+        if linkToClick.accessible_name == "articolo successivo":
+            print("trovato prossimo link da seguire, clicclo " + linkToClick.accessible_name)
+            linkToClick.click()
             return True
+
         else:
             print("Non ci sono altri link 'articolo successivo' nella pagina")
-            #e quindi possiamo fermarci
+            #e quindi possiamo fermarci.
             return False
     except Exception as e:
         print("Errore durante il clic sul link:", e)
         return False
-
-
-'''
-                MAIN
 
 
 
@@ -90,6 +99,5 @@ chrome_options = Options()
 chrome_options.add_argument("webdriver.chrome.driver=" + chromedriver_path)
 # Inizializza il driver di Chrome
 driver = webdriver.Chrome(options=chrome_options)
-takeHTMLFromUrl('https://www.normattiva.it/uri-res/N2Ls?urn:nir:stato:costituzione', driver)
+takeHTMLFromUrl('https://www.normattiva.it/uri-res/N2Ls?urn:nir:stato:regio.decreto:1940-10-28;1443', driver)
 
-'''
